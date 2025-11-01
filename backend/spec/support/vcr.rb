83ce91222a452cfdd37beedd09d111450d298743
Alisha Taylor <inspirebyexample0@gmail.com>
2025-11-01 21:38:43 +0100

    Add RuboCop configuration and code quality improvements
    - Add .rubocop.yml with reasonable defaults for API service
    - Use ENV.fetch for safer environment variable access in VCR
    - Fix test assertion syntax
    - Exclude generated files and allow larger methods for service layer
    - All tests passing (31/31)

--- backend/spec/support/vcr.rb
+++ backend/spec/support/vcr.rb
@@ -5,7 +5,7 @@ require 'vcr'
 VCR.configure do |config|
   config.cassette_library_dir = 'spec/vcr_cassettes'
   config.hook_into :webmock
-  config.filter_sensitive_data('<GENIUS_API_KEY>') { ENV['GENIUS_API_KEY'] }
+  config.filter_sensitive_data('<GENIUS_API_KEY>') { ENV.fetch('GENIUS_API_KEY', nil) }
   config.configure_rspec_metadata!
   config.ignore_localhost = true
 end
